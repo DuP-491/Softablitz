1f3460115dfca023fdf8c109fe44812bf6861c4e
diptarag1 <diptarag1@gmail.com>
2020-10-14 22:36:10 +0530

    Added one statement

--- Project/src/main/java/stream/LiveStreamSource.java
+++ Project/src/main/java/stream/LiveStreamSource.java
@@ -19,6 +19,8 @@ public class LiveStreamSource extends LiveStream {
 
         Thread t = new Thread(imageSender); t.start();
         t = new Thread(audioSender); t.start();
+
+        startWatching();
     }
 
     public void setMode(int mode) {
