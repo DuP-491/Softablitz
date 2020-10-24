07122cb82a54f08f9006f94d9bd86c70cbd17c4a
siddheshpatil777 <siddheshpatil777@gmail.com>
2020-10-25 00:58:50 +0400

    VideoGenerator commenting

--- Project/src/main/java/test/video/VideoGenerator.java
+++ Project/src/main/java/test/video/VideoGenerator.java
@@ -37,37 +37,23 @@ public class VideoGenerator {
 
     private int videoStreamIndex;
     private int audioStreamIndex;
-//    private String outputFilePath = "C:/Users/siddh/Desktop/myVideo.mp4";
     private IMediaWriter writer;
     private int videoHeight=0;
     private int videoWidth=0;
 
-    //    private static TargetDataLine microphone;
-//    private static int audioStreamIndex;
-//    private static int videoStreamIndex;
     VideoGenerator(int videoWidth ,int videoHeight,String outputFilePath){
         writer = ToolFactory.makeWriter(outputFilePath);
         videoStreamIndex=writer.addVideoStream(VIDEO_STREAM_INDEX, 0, VIDEO_OUTPUT_FILE_FORMAT,videoWidth, videoHeight);
         audioStreamIndex=writer.addAudioStream(AUDIO_STREAM_INDEX,1,AUDIO_OUTPUT_FILE_FORMAT,AUDIO_CHANNEL_CONFIG,AUDIO_SAMPLE_RATE);
-        System.out.println("videoStreamIndex "+videoStreamIndex);
-        System.out.println("audioStreamIndex "+audioStreamIndex);
+//        System.out.println("videoStreamIndex "+videoStreamIndex);
+//        System.out.println("audioStreamIndex "+audioStreamIndex);
     }
     public static void main(String[] args) throws AWTException, LineUnavailableException {
-//        screenBounds = Toolkit.getDefaultToolkit().getScreenSize();
-
-//        final IMediaWriter tempWriter = ToolFactory.makeWriter(toutputFilename);
-//        int tvideoStreamIndex=tempWriter.addVideoStream(0, 2, ICodec.ID.CODEC_ID_MPEG4,400, 400);
-//        int taudioStreamIndex=tempWriter.addAudioStream(1,3,ICodec.ID.CODEC_ID_MP3,1,44100);
-//        System.out.println("tvideoStreamIndex "+tvideoStreamIndex);
-//        System.out.println("taudioStreamIndex "+taudioStreamIndex);
-
         AudioFormat format = new AudioFormat(44100.0f, 16, 1, true, true);
         DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
         TargetDataLine microphone = (TargetDataLine) AudioSystem.getLine(info);
         microphone.open(format);
         microphone.start();
- //        AudioFormat audioFormat=new AudioFormat(AudioFormat.Encoding.P)\
-//        TargetDataLine mic=
         VideoGenerator videoGenerator=new VideoGenerator(400,400,"C:/Users/siddh/Desktop/myVideo.mp4");
         long base=System.currentTimeMillis();
         for (long index = 0; index <200; index++) {
@@ -84,6 +70,12 @@ public class VideoGenerator {
 
     }
 
+    /**
+     *  To convert image from normal RGB t BGR Format for xuggler to encode video from Image
+     * @param sourceImage
+     * @param targetType
+     * @return
+     */
     public static BufferedImage convertToType(BufferedImage sourceImage, int targetType) {
         BufferedImage image;
         if (sourceImage.getType() == targetType) {
@@ -96,10 +88,26 @@ public class VideoGenerator {
         }
         return image;
     }
+
+    /**
+     * temporary function to get screen captured image
+     * @param offset
+     * @return
+     * @throws AWTException
+     */
     private static BufferedImage getImage(int offset) throws AWTException {
         BufferedImage image=new Robot().createScreenCapture(new Rectangle(400+offset,400)).getSubimage(offset,0,400,400);
         return image;
     }
+
+    /**
+     * To encode image to Video
+     * Provide an image with its time stamp;
+     * It is important to provide a time stamp as zero while encoding for the first time;
+     *
+     * @param image
+     * @param timeStamp
+     */
     public void encodeImage(BufferedImage image,long timeStamp) {
         BufferedImage imageConvertedToCorrectFormat = convertToType(image, BufferedImage.TYPE_3BYTE_BGR);
         writer.encodeVideo(videoStreamIndex, imageConvertedToCorrectFormat, timeStamp, TimeUnit.MILLISECONDS);
@@ -107,10 +115,7 @@ public class VideoGenerator {
     public void encodeAudio(byte[] bytes){
         if(bytes.length>1) {
             short[] shorts = new short[bytes.length / 2];
-//            System.out.println(shorts.length);
             ByteBuffer.wrap(bytes).order(ByteOrder.BIG_ENDIAN).asShortBuffer().get(shorts);
-//            writer.
-//            ,(long)30*index,TimeUnit.MILLISECONDS
             writer.encodeAudio(audioStreamIndex, shorts);
         }
     }
